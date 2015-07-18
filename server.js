cb8d5ed1eda840fcbaff9ca5e17e8e5611be70d4
Arthur <arthur.p.wright@gmail.com>
2015-07-18 15:04:37 -0400

    Altered http.listen to allow for production port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var router = express.Router();
@@ -13,6 +14,6 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './public', '/index.html'));
 });
 
-http.listen(3000, function(){
-  console.log("Listening on *:3000");
-})
+http.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
